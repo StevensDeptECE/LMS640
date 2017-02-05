44440501e75f04e5cf238d61bca866c061498a19
dippanpatel <dpate74@stevens.edu>
2017-02-05 13:19:31 -0500

    Add files via upload

---
+++ Homework/test2.js
@@ -0,0 +1,30 @@
+myFunction([
+{
+“assignmentId”: “12345678”,
+“studentId”: “1234455”,
+“FileType”: “pde/java/cpp/html”, 
+“dueDay”: [
+	{“Month”: “January”},
+	{“Day”: “29th”},
+	{“Year”: “2017}”
+	{“Time”: “11:59 pm}”
+],
+“courseName”: “CPE640”,
+“url”: “abc.com”,
+“grade”: “0”,
+},
+{
+“assignmentId”: “123456”,
+“studentId”: “12344”,
+“FileType”: “pde/java/cpp/html”, 
+“dueDay”: [
+	{“Month”: “January”},
+	{“Day”: “29th”},
+	{“Year”: “2017”},
+	{“Time”: “11:59 pm”}
+],
+“courseName”: “CPE640”,
+“url”: “xyz.com”,
+“grade”: “0”,
+}
+])
