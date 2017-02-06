0d7f9da801d1e500a32402029025768307660278
Daniel0729 <syin2@stevens.edu>
2017-02-05 22:14:19 -0500

    delete the display_json folder

--- Discussion Forum Chat/display_json/try.js
+++
@@ -1,8 +0,0 @@
-  // 'JSON' data included as above
-   data = '[{"blue" : "is ok", "red" : "is my fave color"}]';
-
-  // Function to 'load JSON' data
-   function load() {
-    var someData_notJSON = JSON.parse(data);
-    console.log(someData_notJSON[0].red); // Will log "is my fave color"
-    }
