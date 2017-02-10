348841290548faf7759aa80b84684f5698f563e9
ybai8 <ybai8@stevens.edu>
2017-02-09 19:01:48 -0500

    page update and json file

---
+++ Discussion Forum Chat/Projectpage-yangbai/js/display.js
@@ -0,0 +1,43 @@
+var project=[
+		{
+			"name": "CSP",
+			"Info": "This is Info",
+			"ID": "01",
+			"member": ["Zhang San","Li Si"],
+			"starttime":"",
+			"endtime": "",
+			"filename": ["file1","file2"],
+			"fileuploadtime": ["",""]
+		}
+		{
+			"name": "Homework",
+			"Info": "This is Info",
+			"ID": "02",
+			"member": ["Zhang San","Li Si"],
+			"starttime":"",
+			"endtime": "",
+			"filename": ["file1","file2"],
+			"fileuploadtime": ["",""]
+		}
+		{
+			"name": "Graph",
+			"Info": "This is Info",
+			"ID": "03",
+			"member": ["Zhang San","Li Si"],
+			"starttime":"",
+			"endtime": "",
+			"filename": ["file1","file2"],
+			"fileuploadtime": ["",""]
+		}
+		{
+			"name": "Discussion forum",
+			"Info": "This is Info",
+			"ID": "04",
+			"member": ["Zhang San","Li Si"],
+			"starttime":"",
+			"endtime": "",
+			"filename": ["file1","file2"],
+			"fileuploadtime": ["",""]
+		}
+	]
+
