81b5ab1dcf2361d2a6b4ed09731e098dcaf19086
soberkiller <fzhao6@stevens.edu>
2017-02-12 10:33:20 -0500

    add js for drawing navbar on main page

--- Discussion Board/disB.js
+++ Discussion Board/disB.js
@@ -18,7 +18,13 @@ function create_div_regular(div_id_tag, div_id, div_class_tag, div_class, div_ev
 	if(div_class_tag == true){
 		cre_element.setAttribute("class", div_class);
 	}
-	target.appendChild(cre_element);
+	if(append_target == "body"){
+		document.body.appendChild(cre_element);
+	}else{
+		target.appendChild(cre_element);
+	}
+	
+
 }
 
 function create_ul_regular(ul_id_tag, ul_id, ul_class_tag, ul_class, ul_event_tag, ul_event_type, ul_event, append_target){
@@ -34,8 +40,12 @@ function create_ul_regular(ul_id_tag, ul_id, ul_class_tag, ul_class, ul_event_ta
 		cre_element.setAttribute("class", ul_class);
 	}
 
-
-	target.appendChild(cre_element);
+	if(append_target == "body"){
+		document.body.appendChild(cre_element);
+	}else{
+		target.appendChild(cre_element);
+	}
+	
 	
 }
 
@@ -51,18 +61,36 @@ function create_li_regular(li_id_tag, li_id, li_class_tag, li_class, li_event_ta
 	if(li_class_tag == true){
 		cre_element.setAttribute("class", li_class);
 	}
-	target.appendChild(cre_element);
+	if(append_target == "body"){
+		document.body.appendChild(cre_element);
+	}else{
+		target.appendChild(cre_element);
+	}
+	
 	
 }
 
-function create_a_regular(a_id_tag, a_id, a_href_tag, a_href, a_node_tag, a_node, append_target){
+function create_a_regular(a_id_tag, a_id, a_data_toggle_tag, a_data_toggle, a_role_tag, a_role, a_aria_haspopup_tag, a_aria_haspopup, a_aria_expanded_tag, 
+						  a_aria_expanded, a_href_tag, a_href, a_node_tag, a_node, append_target){
 	var cre_element = document.createElement("a");
 	var target, node;
 
 	target = document.getElementById(append_target);
 
 	if(a_id_tag == true){
-		cre_element.setAttribute("id", "a_test");
+		cre_element.setAttribute("id", a_id);
+	}
+	if(a_data_toggle_tag == true){
+		cre_element.setAttribute("data-toggle", a_data_toggle);
+	}
+	if(a_role_tag == true){
+		cre_element.setAttribute("role", a_role);
+	}
+	if(a_aria_haspopup_tag == true){
+		cre_element.setAttribute("aria-haspopup", a_aria_haspopup);
+	}
+	if(a_aria_expanded_tag == true){
+		cre_element.setAttribute("aria-expanded", a_aria_expanded);
 	}
 	if(a_href_tag == true){
 		cre_element.setAttribute("href", a_href);
@@ -71,8 +99,30 @@ function create_a_regular(a_id_tag, a_id, a_href_tag, a_href, a_node_tag, a_node
 		node = document.createTextNode(a_node);
 		cre_element.appendChild(node);
 	}
+	if(append_target == "body"){
+		document.body.appendChild(cre_element);
+	}else{
+		target.appendChild(cre_element);
+	}
+}
+	function create_nav_regular(nav_id_tag, nav_id, nav_class_tag, nav_class, append_target){
+		var cre_element = document.createElement("nav");
+		var target;
 
-	target.appendChild(cre_element);
+		target = document.getElementById(append_target);
+
+		if(nav_id_tag == true){
+			cre_element.setAttribute("id", nav_id);
+		}
+		if(nav_class_tag == true){
+			cre_element.setAttribute("class", nav_class);
+		}
+		if(append_target == "body"){
+			document.body.appendChild(cre_element);
+		}else{
+			target.appendChild(cre_element);
+		}
+		
+	}
 
 	
-}
