56f2b2ef2bcc4efc41742f7facecce4f662355d6
FUJICJK <junkai.cui.914@gamil.com>
2017-02-14 13:30:07 -0500

    seperate my js file

---
+++ LiquiZ/assets/javascript/Junkai.js
@@ -0,0 +1,25 @@
+function Match(types,choices, id) {
+    this.id = id;
+    this.types=types;
+    this.choices = choices;
+    this.responses = [];
+}
+
+Match.prototype.draw = function(div) {
+    for (var j=0;j<this.types.length;j++){
+        var x = document.createElement('div');
+        var selectList = document.createElement("select");
+        for (var i = 0; i < this.choices.length; i++) {
+            var option = document.createElement('option');
+            option.value = this.choices[i];
+            option.text = this.choices[i];
+            selectList.appendChild(option);
+        }
+        var label = document.createElement('label');
+        label.appendChild(document.createTextNode(this.types[j]));
+        x.appendChild(label);
+        x.appendChild(selectList);
+
+        div.appendChild(x);
+    }
+}
--- LiquiZ/assets/javascript/questions.js
+++ LiquiZ/assets/javascript/questions.js
@@ -43,31 +43,7 @@ MCDrop.prototype.draw = function(div) {
     div.appendChild(x);
 }
 
-function Match(types,choices, id) {
-    this.id = id;
-    this.types=types;
-    this.choices = choices;
-    this.responses = [];
-}
 
-Match.prototype.draw = function(div) {
-    for (var j=0;j<this.types.length;j++){
-        var x = document.createElement('div');
-        var selectList = document.createElement("select");
-        for (var i = 0; i < this.choices.length; i++) {
-            var option = document.createElement('option');
-            option.value = this.choices[i];
-            option.text = this.choices[i];
-            selectList.appendChild(option);
-        }
-        var label = document.createElement('label');
-        label.appendChild(document.createTextNode(this.types[j]));
-        x.appendChild(label);
-        x.appendChild(selectList);
-
-        div.appendChild(x);
-    }
-}
 
 function Matrix(rows, cols, id) {
 	this.id = id;
