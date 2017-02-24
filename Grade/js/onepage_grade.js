7a21095939f2a3a3392932a1bc349a3faaf0b40c
Qihang Chen <qchen10@stevens.edu>
2017-02-24 13:41:46 -0500

    onepage

--- Grade/js/onepage_grade.js
+++ Grade/js/onepage_grade.js
@@ -16,9 +16,10 @@ Grade.prototype.draw = function(c) {
 
   clearElements("up3");
   var newDiv = Util.div("", "wrapper");
-  var newTable = Util.table(this.payload, true, "gradeTable", "keywords");
+  var newTable = createGradeTable(this.payload, "keywords")
   newDiv.appendChild(newTable);
   c.appendChild(newDiv);
+  $("#keywords").tablesorter();
   clearClass("active");
   //set active to some other class
 }
@@ -30,3 +31,42 @@ var gradepayload2 = [
 ["John", 104083, 66, 76, 98],
 ["Kelly", 123456, 87, 88, 98],
 ];
+
+function createGradeTable(payload, id) {
+  var table = document.createElement("table");
+  table.setAttribute("id",id);
+  var header = document.createElement("thead");
+  var body = document.createElement("tbody");
+  var row = document.createElement("tr");
+
+  for (var i in payload[0]) {
+    var data = document.createElement("th");
+    var span = document.createElement("span");
+    row.appendChild(data);
+    data.appendChild(span)
+    span.innerHTML=payload[0][i];
+}
+
+for (var i = 1; i < payload.length; ++i) {
+  var data_row = document.createElement("tr");
+  data_row.setAttribute("class","lalign");
+    body.appendChild(data_row);
+    var name = document.createElement("td");
+    var id = document.createElement("td");
+    data_row.appendChild(name);
+    data_row.appendChild(id);
+    name.innerHTML=payload[i][0];
+    id.innerHTML=payload[i][1];
+
+    for (var j = 2; j < payload[i].length; ++j) {
+      var score = document.createElement("td");
+      data_row.appendChild(score);
+      score.innerHTML = payload[i][j];
+    }
+}
+
+  table.appendChild(header);
+  table.appendChild(body);
+  header.appendChild(row);
+  return table;
+}
