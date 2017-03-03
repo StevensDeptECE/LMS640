742f5c2326890fdfc353154b47bb6643a8989aeb
yucheng <yxie14@stevens.edu>
2017-03-03 12:37:56 -0500

    Merge branch 'master' of /Users/yucheng/Documents/Intellij/640project/LMS640 with conflicts.

--- LiquiZ/assets/javascript/addQuiz.js
+++ LiquiZ/assets/javascript/addQuiz.js
@@ -77,8 +77,9 @@ function add_row()
     document.getElementById("new_oper").value="";
 }
 
+var datas = [];
 function tableToJson(table) {
-    var datas = []; // first row needs to be headers
+    // var datas = []; // first row needs to be headers
     var headers = ["id","title","comp"];
     // for (var i=0; i<table.rows[0].cells.length; i++) {
     //     headers[i] = table.rows[0].cells[i].innerHTML.toLowerCase().replace(/ /gi,'');
@@ -106,3 +107,222 @@ function tableToJson(table) {
     }
     return datas;
 }
+
+// -----------------------------------------------------------------------
+
+Util = {
+    subClass: function (sup, sub) {
+        sub.prototype = Object.create(sup.prototype);
+        sub.prototype.constructor = sub;
+    }
+};
+
+
+// Display element can draw itself into a div box,
+// has optional class
+function Display(id, clas) {
+    this.div;
+    this.id = id;
+    this.class = clas; // could be undefined?
+}
+
+function app(div, text) {
+    div.appendChild(document.createTextNode(text));
+}
+
+function app2(div, text) {
+    div.appendChild(document.createElement(text));
+}
+
+// make a div under the parent. This is a utility function for display
+// objects but not every display object necessarily creates a div
+Display.prototype.md = function(parent) {
+    this.div = document.createElement('div');
+    if (this.class)
+        this.div.class = this.class;
+    parent.appendChild(this.div);
+}
+
+
+// this method should be overridden by all children
+Display.prototype.disp = function(div) {
+    app(div, this.constructor.name + ':');
+}
+
+
+function Prefs() {
+
+}
+
+Prefs.prototype.getPolicy = function(json) {
+    var p = this.policies[json.policy ? json.policy : "default"];
+}
+
+function Response() {
+
+
+}
+
+Util.subClass(Display, Response);
+
+function Prefs() {}
+
+Prefs.prototype.getPolicy = function(id) {
+
+}
+
+Prefs.prototype.getRegex = function(id) {
+
+}
+
+var prefs = new Prefs();
+
+function Answer(id) {
+
+}
+
+Util.subClass(Display, Answer);
+
+function StringAnswer(s, id) {
+    this.id = id; // do in parent
+    this.s = s;
+}
+
+StringAnswer.prototype.draw = function(div) {
+    app(div, this.s);
+}
+
+function Instr(s, id) {
+    this.id = id;
+    this.s = s;
+}
+
+Instr.prototype.draw = function(div) {
+    app(div, this.s);
+}
+
+function Eqn(s, id) {
+    this.id = id;
+    this.s = s;
+}
+
+Eqn.prototype.draw = function(div) {
+    app(div, this.s);
+}
+
+function Eqn2(s, id) {
+    this.id = id;
+    this.s = s;
+}
+
+Eqn2.prototype.draw = function(div) {
+    app2(div, this.s);
+}
+
+function QC(parent, json) {
+    this.id = json.id;
+    this.title = json.title;
+    //TODO: inherit default from quiz, then from user (not 1)
+    this.points = (typeof json.points === 'undefined') ? 1 : json.points;
+    this.level = (typeof json.level === 'undefined') ? 1 : json.level;
+    this.md(parent);
+    this.div.className = 'qc';
+    this.comp = [];
+    for (var i = 0; i < json.comp.length; ++i) {
+        var comp = json.comp[i];
+        var c = "new " + comp[0] + "(";
+        for(var j = 1;  j < comp.length; j++) {
+            var value = comp[j];
+            console.log(value);
+            if (typeof(value) === 'string')
+                c += "'" + comp[j] + "'";
+            else if(comp[j].constructor === Array) {
+                console.log(JSON.stringify(comp[j]));
+                c+= JSON.stringify(comp[j]);
+            }
+            else
+                c += comp[j];
+            if(j != comp.length-1)
+                c += ', ';
+        }
+        c += ')';
+        //c = "new " + comp[0] + "(";  + comp[2] + "' , '" + comp[1] + "')"; //need to loop this for more than 3 elements in array?
+
+        console.log(c);
+        this.comp.push(eval(c));
+    }
+}
+
+Util.subClass(Display, QC);
+
+QC.prototype.buildHeader = function() { //is this needed?
+    var header = document.createElement('div');
+    header.className = 'header';
+    var headerString = this.id + ': ' + this.title;
+    header.appendChild(document.createTextNode(headerString));
+    return header;
+}
+
+QC.prototype.draw = function() {  //need to pass in some kind of element to draw onto?
+    var d = this.buildHeader();
+    this.div.appendChild(d);
+    console.log(this);
+    for (var i = 0; i < this.comp.length; ++i)
+        this.comp[i].draw(this.div); //pass in div
+}
+
+
+function Quiz(parent, json) {
+
+    for (var k in json) {
+        this[k] = json[k];
+    }
+    console.log(this);
+    this.md(parent);
+    //parent.appendChild(this.div);
+    this.policy = prefs.getPolicy(json);
+    for (var i = 0; i < this.questions.length; ++i) {
+        console.log(this.questions[i]);
+        this.questions[i] = new QC(this.div, this.questions[i]);
+        console.log(this.questions[i]);
+    }
+    console.log(this);
+}
+Util.subClass(Display, Quiz);
+
+//add question container
+Quiz.prototype.add = function(qc) {
+    this.questions.push(qc);
+}
+
+Quiz.prototype.drawQuiz = function() {
+    console.log(this);
+    console.log(this.questions.length);
+    for (var i = 0; i < this.questions.length; ++i) {
+        console.log(this.questions[i].draw); //this is thinking to do quiz.draw??
+        this.questions[i].draw();
+    }
+}
+
+
+function load() {
+    var p = document.getElementById("content");
+    console.log(p);
+
+    var test = [1,2,3,4];
+    console.log(typeof(test));
+    console.log(test);
+
+
+    var quest =  datas;
+    var json = {
+        title: "test",
+        class: "L-quiz",
+        questions: quest
+    }
+    var q = new Quiz(p, json);
+
+    q.drawQuiz();
+}
+
+
