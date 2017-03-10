92c7f5600c923e046389a253d446ea795cd19af5
Matthew Melachrinos <mmelach@gmail.com>
2017-03-10 14:32:06 -0500

    updating settings

--- LiquiZ/assets/javascript/Settings.js
+++ LiquiZ/assets/javascript/Settings.js
@@ -1,40 +1,44 @@
-function load() {
-	var p = document.getElementById("content");
-	console.log(p);
-	
-	var test = [1,2,3,4];
-	console.log(typeof(test));
-	console.log(test);
-	
-	
-	var settings = [
-
-
-{
-    id: "qc1008",
-	title: "Addition",
-	comp: [
-		["Instr", "What is ", "1"],
-		["Eqn", "2+2", "2"],
-		["MCDrop", [1,2,3,4], "3"]
-	]
-},
-
-{
-	id: "qc1010",
-	title: "Match",
-	comp: [
-		["Instr", "Match the types", "1"],
-		["Match",["animal","number","food"],["ice cream", "dog", "three"], "2" ]
-	]
+function settings(payload){
+    this.payload = payload;
 }
-    ];
-	var json = {
-  		title: "test",
- 		class: "L-quiz",
- 		questions: quest
-	}
-	var q = new Quiz(p, json);
+settings.prototype.draw = function(s) {
+    clearElements("up3");
+    clearElements("up2");
+    var header = Util.h1("Settings","h02");
+    document.getElementById("up2").appendChild(header);
+    
+
+	var p = document.getElementById("up3");
+	
+	var q = new QC(p, settingsPayload);
 
 	q.drawQuiz();
+    clearClass("active");
+    document.getElementById("settings").className = "active";
 }
+
+
+var settingsPayload = [
+    title: "Settings",
+    class: "L-quiz",
+    questions: 
+
+        {
+            id: "qc1008",
+            title: "Addition",
+            comp: [
+                ["Instr", "What is ", "1"],
+                ["Eqn", "2+2", "2"],
+                ["MCDrop", [1,2,3,4], "3"]
+            ]
+        },
+
+        {
+            id: "qc1010",
+            title: "Match",
+            comp: [
+                ["Instr", "Match the types", "1"],
+                ["Match",["animal","number","food"],["ice cream", "dog", "three"], "2" ]
+            ]
+        }
+    ];
