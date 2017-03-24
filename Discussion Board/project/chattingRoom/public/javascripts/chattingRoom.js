02736e1b0694a2f2f8c3c96d5924d8b136eef04d
soberkiller <fzhao6@stevens.edu>
2017-03-23 20:15:28 -0400

    putting angularJS code into html

--- Discussion Board/project/chattingRoom/public/javascripts/chattingRoom.js
+++ Discussion Board/project/chattingRoom/public/javascripts/chattingRoom.js
@@ -1,6 +1,12 @@
 var app = angular.module('chatRoom', [])
-app.controller('myCtrl',function($scope){
+app.controller('chatContentCtrl',function($scope){
+    $scope.names = [
+        {name:'John', text:'Hello', createBy:'12:31'},
+        {name:'Killer', text:'World', createBy:'12:32'},
+        {name:'Kevin', text:'world',createBy:'12:33'},
+        {name:'Martha', text:'hello',createBy:'12:34'}
+    ];
+    $scope.chat=[];     // this is going to store every chat from others!! 
+    $scope.newChat={createBy:'', text:'', createAt:''}
     
-    
-    
-})
+});
