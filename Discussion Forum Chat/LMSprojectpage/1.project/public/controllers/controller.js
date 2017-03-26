d6ab7dd3ff5b9ed30c8a370c51994c3f01eefd42
Bhavitha Dasaram <bdasaram@stevens.edu>
2017-03-25 23:17:34 -0400

    update

--- Discussion Forum Chat/LMSprojectpage/1.project/public/controllers/controller.js
+++ Discussion Forum Chat/LMSprojectpage/1.project/public/controllers/controller.js
@@ -1,4 +1,20 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngRoute']);
+  
+myApp.config(function($routeProvider) {
+  $routeProvider
+    .when('/home', {
+      templateUrl: 'project.html',
+      controller: 'AppCtrl'
+    })
+    .when('/info', {
+      templateUrl: 'info.html',
+      controller: 'AppCtrl'
+    })
+    .otherwise({
+      redirectTo: '/home'
+    });
+});
+
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
     console.log("Hello World from controller");
     
@@ -6,6 +22,16 @@ myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
     $scope.myFunc = function() {
         $scope.showMe = !$scope.showMe;
     };
+
+    $scope.uploadFile = function(){
+               var file = $scope.myFile;
+               
+               console.log('file is ' );
+               console.dir(file);
+               
+               var uploadUrl = "/fileUpload";
+               fileUpload.uploadFileToUrl(file, uploadUrl);
+            };
 
 
 var refresh = function() {
@@ -35,6 +61,13 @@ $scope.remove = function(id) {
 };
 
 $scope.edit = function(id) {
+  console.log(id);
+  $http.get('/projectlist/' + id).success(function(response) {
+    $scope.project = response;
+  });
+};
+
+$scope.get = function(id) {
   console.log(id);
   $http.get('/projectlist/' + id).success(function(response) {
     $scope.project = response;
