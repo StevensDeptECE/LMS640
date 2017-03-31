1b782f416f86878447c3c6ad04632680e5884b94
soberkiller <fzhao6@stevens.edu>
2017-03-30 22:26:21 -0400

    add method to let server broadcast message to every person who is online. This can also be used in existing chatting room framework

--- Discussion Board/project/chattingRoom/public/javascripts/chat-controller.js
+++ Discussion Board/project/chattingRoom/public/javascripts/chat-controller.js
@@ -1,7 +1,42 @@
 (function(window, angular, undefined){
     angular.module('chatApp')
-        .controller("chatCtrl", ['$scope', function($scope){
+        .controller("chatCtrl", ['$rootScope', '$scope', function($rootScope, $scope){
             var vm = this;
-            vm.test = "Yes chat is created!";
+            var socket = window.io('localhost:3000/');
+            vm.date = new Date().toTimeString().substr(0, 8);
+
+            vm.newMessage = undefined;
+            vm.messages=[];
+                socket.on("receive-message", function(msg){
+
+                    $scope.$apply(function(){
+                                            console.log("received message", msg);
+                        vm.messages.push(msg);
+                    });
+                    
+                });
+            
+            vm.username = undefined;
+            vm.sendMessage = function(){
+                var newMessage = {
+                    username: vm.username,
+                    message: vm.newMessage,
+                    date: vm.date
+                };
+                socket.emit("new-message", newMessage);
+                vm.newMessage = undefined;
+            };
+            
+            
+            $rootScope.$on('modern-user', function(event, data){
+                vm.username = data;                
+            });
+            $scope.$watch(function(){
+                return vm.username;
+            }, function(){
+                if(vm.username){
+                    console.log("this is value for username", vm.username);
+                }
+            })
         }])
 })(window, window.angular);
