b28fcbcfc56010982dffef97c9f667aba6019c54
stRAWRberry <ntran@stevens.edu>
2017-04-05 03:42:55 -0400

    why won't you make buttons??

--- Calendar/calendar_onepage/newwithdrawfunction.js
+++ Calendar/calendar_onepage/newwithdrawfunction.js
@@ -1,237 +1,217 @@
-	var holiday = [
+var holiday = [
 	{ "style":"holiday","name":"Christmas","notes":"Merry Christmas!","day":25,"month":12,"year":2017},
 	{ "style":"holiday","name":"President's Day","notes":"Have a good day!","day":20,"month":2,"year":2017},
 	{ "style":"mid","name":"EE575 mid-exam","notes":"EE575 midterm exam","day":10,"month":3,"year":2017},
 	{ "style":"quiz","name":"EE552 test2","notes":"EE552 test2","day":20,"month":3,"year":2017},
 	{ "style":"class","name":"CPE-640","notes":"CPE-640 12:00-14:30","day":24,"month":3,"year":2017}];
 
-    var tempDate;
+var tempDate;
 
-    function getRightNow() {
-        return new Date();
+function getRightNow() {
+	return new Date();
+}
+
+function getDay(date){
+	var day = date.getDay();
+  if(0 == day){
+  	day = 7;
+  }
+  return day;
+}
+
+function getDays(date) {
+	var month = date.getMonth() + 1;
+  var currentDate = date.getDate();
+  date.setMonth(month, 0);
+  var days = date.getDate();
+  date.setMonth(month - 1, currentDate);
+  return days;
+}
+
+function getFirstDayOfMonth(date){
+	var currentDate = date.getDate();
+  date.setDate(1);
+  var firstDayOfMonth = getDay(date);
+  date.setDate(currentDate);
+  return firstDayOfMonth;
+}
+
+function getLastDayOfMonth(date){
+	var currentDate = date.getDate();
+  var days = getDays(date);
+  date.setDate(days);
+  var lastDayOfMonth = getDay(date);
+  date.setDate(currentDate);
+  return lastDayOfMonth;
+}
+
+function getHeadOfNextMonth(date) {
+	var days = getDays(date);
+  var firstDayOfMonth = getFirstDayOfMonth(date);
+  var verbose = firstDayOfMonth;
+  var row = "";
+  var line = 0;
+
+  for (var i = 1; i <= days; i++) {
+  	if ((i + verbose) % 7 == 0) {
+    	line++;
     }
+  }
 
-    function getDay(date){
-        var day = date.getDay();
-        if(0 == day){
-            day = 7;
-        }
-        return day;
+  var year = date.getFullYear();
+  var month = date.getMonth();
+  if (month == 11) {
+  	year += 1;
+    month = 0;
+  }
+  else {
+  	month += 1;
+  }
+
+  var nextMonth = new Date(year, month);
+  var firstDayOfNextMonth = getFirstDayOfMonth(nextMonth);
+  verbose = firstDayOfNextMonth;
+
+  if (line == 5 || line == 4) {
+  	for (var i = 1; i <= 7 - verbose; i++) {
+    	row += "<td class='next'>"+i+"</td>";
     }
+    row += "</tr>";
+  }
+  return row;
+}
 
-    function getDays(date) {
-        var month = date.getMonth() + 1;
-        var currentDate = date.getDate();
+function getEndOfPreMonth(date){
+	var year = date.getFullYear();
+  var month = date.getMonth();
+  if (month == 0) {
+  	year -= 1;
+    month = 11;
+  }
+  else {
+  	month -= 1;
+  }
+  var preMonth = new Date(year, month);
+  var days = getDays(preMonth);
+  var firstDayOfMonth = getFirstDayOfMonth(preMonth);
+  var verbose = firstDayOfMonth;
+  var row = "";
 
-        date.setMonth(month, 0);
-        var days = date.getDate();
+  for (var i = 1; i <= days; i++) {
+  	row += "<td class='pre'>" + i + "</td>";
 
-        date.setMonth(month - 1, currentDate);
-
-        return days;
+    if ((i + verbose) % 7 == 0) {
+    	row = "";
     }
+  }
+  return row;
+}
 
-    function getFirstDayOfMonth(date){
-        var currentDate = date.getDate();
-        date.setDate(1);
-        var firstDayOfMonth = getDay(date);
-        date.setDate(currentDate);
-        return firstDayOfMonth;
-    }
-
-    function getLastDayOfMonth(date){
-        var currentDate = date.getDate();
-        var days = getDays(date);
-        date.setDate(days);
-        var lastDayOfMonth = getDay(date);
-        date.setDate(currentDate);
-        return lastDayOfMonth;
-    }
-
-    function getHeadOfNextMonth(date) {
-        var days = getDays(date);
-        var firstDayOfMonth = getFirstDayOfMonth(date);
-        var verbose = firstDayOfMonth;
-        var row = "";
-        var line = 0;
-
-        for (var i = 1; i <= days; i++) {
-            if ((i + verbose) % 7 == 0) {
-                line++;
-            }
-        }
-
-        var year = date.getFullYear();
-        var month = date.getMonth();
-        if (month == 11) {
-            year += 1;
-            month = 0;
-        }
-        else {
-            month += 1;
-        }
-
-        var nextMonth = new Date(year, month);
-        var firstDayOfNextMonth = getFirstDayOfMonth(nextMonth);
-        verbose = firstDayOfNextMonth;
-
-        if (line == 5 || line == 4) {
-            for (var i = 1; i <= 7 - verbose; i++) {
-                row += "<td class='next'>"+i+"</td>";
-            }
-            row += "</tr>";
-        }
-        return row;
-    }
-
-    function getEndOfPreMonth(date){
-        var year = date.getFullYear();
-        var month = date.getMonth();
-        if (month == 0) {
-            year -= 1;
-            month = 11;
-        }
-        else {
-            month -= 1;
-        }
-        var preMonth = new Date(year, month);
-        var days = getDays(preMonth);
-        var firstDayOfMonth = getFirstDayOfMonth(preMonth);
-        var verbose = firstDayOfMonth;
-        var row = "";
-
-        for (var i = 1; i <= days; i++) {
-            row += "<td class='pre'>" + i + "</td>";
-
-            if ((i + verbose) % 7 == 0) {
-                row = "";
-            }
-        }
-        return row;
-    }
-
-    function show(customDate) {
-        var today = getRightNow();
-		var date = customDate.getDate();
-        var days = getDays(customDate);
-        var firstDayOfMonth = getFirstDayOfMonth(customDate);
-        var verbose = firstDayOfMonth;
-        var dateString = "";
-        dateString += "<table><th>SUN</th><th>MON</th><th>TUE</th><th>WED</th><th>THU</th><th>FRI</th><th>SAT</th>";
-        var row = "";
-        row = getEndOfPreMonth(customDate);
-        for (var i = 1; i <= days; i++) {
-            if(i == today.getDate() && customDate.getMonth() == today.getMonth() && customDate.getFullYear() == today.getFullYear()){
-                row += "<td class='today'>" + i;
-								console.log(holiday.length);
-				for(var j = 0; j < holiday.length; j++){
-					if( i == holiday[j].day && customDate.getMonth() + 1 == holiday[j].month && customDate.getFullYear() == holiday[j].year)
+function show(customDate) {
+	var today = getRightNow();
+	var date = customDate.getDate();
+	var days = getDays(customDate);
+	var firstDayOfMonth = getFirstDayOfMonth(customDate);
+	var verbose = firstDayOfMonth;
+	var dateString = "";
+	dateString += "<table><th>SUN</th><th>MON</th><th>TUE</th><th>WED</th><th>THU</th><th>FRI</th><th>SAT</th>";
+	var row = "";
+	row = getEndOfPreMonth(customDate);
+	for (var i = 1; i <= days; i++) {
+		if(i == today.getDate() && customDate.getMonth() == today.getMonth() && customDate.getFullYear() == today.getFullYear()){
+			row += "<td class='today'>" + i;
+			for(var j = 0; j < holiday.length; j++){
+				if( i == holiday[j].day && customDate.getMonth() + 1 == holiday[j].month && customDate.getFullYear() == holiday[j].year)
 					row += '\n' + "<button class='createholiday' onclick='createwindow("+j+")'>"+holiday[j].name+"</button>";
-				}
-				row += "</td>";
-            }
-            else{
-                row += "<td class='current'>" + i;
-				for(var j = 0; j < 5; j++){
-					if( i == holiday[j].day && customDate.getMonth() + 1 == holiday[j].month && customDate.getFullYear() == holiday[j].year)
+					console.log("i made the holiday " + holiday[j].name);
+			}
+			row += "</td>";
+		}
+		else{
+			row += "<td class='current'>" + i;
+			for(var j = 0; j < 5; j++){
+				if( i == holiday[j].day && customDate.getMonth() + 1 == holiday[j].month && customDate.getFullYear() == holiday[j].year)
 					row += '\n'+ "<button class='createholiday' onclick='createwindow("+j+")'> " + holiday[j].name + " </button>";
-				}
-				row += "</td>";
-            }
-            if ((i + verbose) % 7 == 0) {
-                dateString += "<tr>" + row + "</tr>";
-                row = "";
-            }
-        }
+			}
+			row += "</td>";
+		}
+		if ((i + verbose) % 7 == 0) {
+			dateString += "<tr>" + row + "</tr>";
+			row = "";
+		}
+	}
 
-        var returnRow = getHeadOfNextMonth(customDate);
+	var returnRow = getHeadOfNextMonth(customDate);
 
-        dateString += "<tr>" + row + returnRow;
-        dateString += "</table>";
+	dateString += "<tr>" + row + returnRow;
+	dateString += "</table>";
 
-				var calendarContainer = document.getElementById("up3");
-        calendarContainer.innerHTML = dateString;
+	var calendarContainer = document.getElementById("up3");
+	calendarContainer.innerHTML = dateString;
+}
 
-    }
+function createwindow(n){
+	var para = document.createElement("div");
+	para.setAttribute("class","divholiday");
+	para.setAttribute("id", "divholiday");
+  var node = document.createTextNode(holiday[n].notes);
+  para.appendChild(node);
+  document.body.appendChild(para);
+}
 
-    function createwindow(n){
-    	var para = document.createElement("div");
-		para.setAttribute("class","divholiday");
-		para.setAttribute("id", "divholiday");
-    	var node = document.createTextNode(holiday[n].notes);
-    	para.appendChild(node);
-    	document.body.appendChild(para);
-    }
-
-
-	/*function createEventwindow(){
-    	var para = document.createElement("div");
-		para.setAttribute("class","event");
-		para.setAttribute("id", "event");
-    	var node = document.createTextNode("please add your event");
-    	para.appendChild(node);
-    	document.body.appendChild(para);
-		var linktest = document.createElement("a");
-		linktest.setAttribute("href","http://my.stevens.edu");
-		node = document.createTextNode("MyStevens");
-		linktest.appendChild(node);
-		document.getElementById("event").appendChild(linktest);
-		console.log("i made a window");
-    }*/
-
-	document.onclick = function(e) {
-		if(document.getElementById("divholiday")){
-			if(e.target.id != "divholiday" && e.target.className != "createholiday") {
+document.onclick = function(e) {
+	if(document.getElementById("divholiday")){
+		if(e.target.id != "divholiday" && e.target.className != "createholiday") {
 			var holiday = document.getElementById("divholiday");
 			holiday.parentNode.removeChild(holiday);
 			//console.log("i took away the window");
 		}
-		}
 	}
+}
 
 
-		function setCalendar() {
-        show(getRightNow());
+function setCalendar() {
+	show(getRightNow());
         //changeWeekendStyle();
-				drawCalendarButtons();
-    }
+	drawCalendarButtons();
+}
 
-    function getTempDate(){
-        if(tempDate == undefined) {
-            tempDate = getRightNow();
-        }
-        return tempDate;
-    }
+function getTempDate(){
+	if(tempDate == undefined) {
+  	tempDate = getRightNow();
+  }
+  return tempDate;
+}
 
-    function getPreMonth() {
-        tempDate = getTempDate();
+function getPreMonth() {
+	tempDate = getTempDate();
 
-        var year = tempDate.getFullYear();
-        var month = tempDate.getMonth();
-        var date = tempDate.getDate();
+  var year = tempDate.getFullYear();
+  var month = tempDate.getMonth();
+  var date = tempDate.getDate();
 
-        if (month == 0) {
-            year -= 1;
-            tempDate.setFullYear(year, 11, 1);
-        }
-        else {
-            month -= 1;
-            tempDate.setMonth(month, 1);
-        }
-        var preDays = getDays(tempDate);
-        if(date >= preDays){
-            date = preDays;
-        }
-        tempDate.setDate(date);
-        return tempDate;
-    }
+  if (month == 0) {
+  	year -= 1;
+    tempDate.setFullYear(year, 11, 1);
+  }
+  else {
+  	month -= 1;
+    tempDate.setMonth(month, 1);
+  }
+  var preDays = getDays(tempDate);
+  if(date >= preDays){
+  	date = preDays;
+  }
+  tempDate.setDate(date);
+  return tempDate;
+}
 
-    function preButton(){
-        var preMonth = getPreMonth();
-        show(preMonth);
-				drawCalendarButtons();
+function preButton(){
+	var preMonth = getPreMonth();
+  show(preMonth);
+	drawCalendarButtons();
         //changeWeekendStyle();
-    }
+}
 
 
     function resume(){
@@ -320,7 +300,6 @@
 
 		    clearElements("up3");
 		    show(getRightNow());
-				console.log(holiday);
 		    //changeWeekendStyle();
 		    drawCalendarButtons();
 		    //onclickClass("active", launch)
@@ -466,7 +445,8 @@
 				holiday.push(data);
 				console.log(data);
 				popup.style.display = "none";
-				drawCalendar();
+				show(tempDate);
+				drawCalendarButtons();
 			}
 			var form = document.getElementsByClassName("holidayForm")[0];
 			form.addEventListener('submit', handleFormSubmit);
