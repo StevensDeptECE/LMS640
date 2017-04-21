2f14e32a105460d078bc15c1eff1e0c8e0e9266e
ybai8 <ybai8@stevens.edu>
2017-04-21 13:49:52 -0400

    combine upload and page

--- Discussion Forum Chat/LMSprojectpage/1.project/server.js
+++ Discussion Forum Chat/LMSprojectpage/1.project/server.js
@@ -5,8 +5,32 @@ var app = express();
 var mongojs = require('mongojs');
 var db = mongojs('projectlist', ['projectlist']);
 var bodyParser = require('body-parser');
+var path = require('path');
+var http = require('http');
+var multer  = require('multer');
+
+var storage = multer.diskStorage({
+  destination: './uploads/',
+  filename: function (req, file, cb) {
+    cb(null, file.originalname.replace(path.extname(file.originalname), "") + '-' + Date.now() + path.extname(file.originalname))
+  }
+})
+
+var upload = multer({ storage: storage })
 
 app.use(express.static(__dirname + '/public'));
+app.set('port', process.env.PORT || 3000);
+
+
+app.post('/savedata', upload.single('file'), function(req,res,next){
+    console.log('Uploade Successful ', req.file, req.body);
+});
+
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
 app.use(bodyParser.json());
 
 app.get('/projectlist', function (req, res) {
@@ -63,5 +87,5 @@ app.put('/projectlist/:id', function (req, res) {
   );
 });
 
-app.listen(3000);
+//app.listen(3000);
 console.log("Server running on port 3000");
