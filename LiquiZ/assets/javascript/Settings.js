b9d890eb02fd5cbda79eecb7f92585f6b9f903dd
cbean03 <cbean03@gmail.com>
2017-05-11 22:37:53 -0400

    update settings page to remove things specifc to takeQuiz

--- LiquiZ/assets/javascript/Settings.js
+++ LiquiZ/assets/javascript/Settings.js
@@ -7,15 +7,18 @@ settings.prototype.draw = function(s) {
     var header = Util.h1("Settings","h02");
     document.getElementById("up2").appendChild(header);
     
-
 	var p = document.getElementById("up3");
 	console.log(settingsPayload);
 	
 	var q = new takeQuiz(settingsPayload);
 
 	q.draw(p);
-    clearClass("active");
-    document.getElementById("settings").className = "active";
+    document.getElementById("submitQuiz-button").innerHTML = "Save";
+    document.getElementsByClassName("quiz-nav-right")[0].style = "display:none";
+    var qc = document.getElementsByClassName("qc");
+    for(var i = 0; i < qc.length; i++){
+        document.getElementsByClassName("qcPoints")[0].remove();
+    }
 }
 
 
@@ -51,10 +54,10 @@ var settingsPayload = {
                 ["Instr", "Will you require an access code to open the quiz?", "1"],
                 [ "MC",["Yes", "No"], "2"],
                 ["Instr", "Would you like to automatically publish new quizzes?", "1"],
-                [ "MC",["Yes", "No"], "2"],
-                ["Instr", "What is the standard amount of time between quiz open date and quiz due date?", "1"],
+                [ "MC",["Yes", "No"], "3"],
+                ["Instr", "How many days between quiz open date and quiz due date?", "1"],
                 [ "FillIn", "2"],
-                ["Instr", "What is the standard amount of time between quiz due date and quiz close date?", "1"],
+                ["Instr", "How many days between quiz due date and quiz close date?", "1"],
                 [ "FillIn", "2"]
             ]
         }
